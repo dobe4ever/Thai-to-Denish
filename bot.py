1d2045a5097c0a2412dee8044b37296d81e8e2e4
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2023-11-18 23:53:25 +0000

    f

--- bot.py
+++ bot.py
@@ -24,14 +24,11 @@ def handle_all(update: Update, context):
 
     if update.message.audio:
         path=download_file(bot.get_file(update.message.audio.file_id))
-        print(f"\npath: {path}")
-        resp1=translation(path)
-        print(f"\nresp1: {resp1}")
-        resp2=transcription(path)
-        print(f"\nresp2: {resp2}")
+        resp1=transcription(path)
+        resp2=translation(path)
 
         bot.send_message(text=f"Translation 1:\n\n{resp1}", chat_id=userid, parse_mode="Markdown")
-        
+    
         bot.send_message(text=f"Translation 2:\n\n{resp2}", chat_id=userid, parse_mode="Markdown")
     
     elif update.message.voice.file_id:
@@ -39,49 +36,19 @@ def handle_all(update: Update, context):
         resp1=transcription(path)
         resp2=translation(path)
 
+        bot.send_message(text=f"Translation 1:\n\n{resp1}", chat_id=userid, parse_mode="Markdown")
+
+        bot.send_message(text=f"Translation 2:\n\n{resp2}", chat_id=userid, parse_mode="Markdown")
+
     else:
         bot.send_message(text="Please send audio", chat_id=userid)
-
-
-# def handle_audio(update: Update, context):
-#     userid = update.message.from_user.id
-#     if str(userid) not in admins: 
-#         return
-    
-#     file = bot.get_file(update.message.audio.file_id)
-    
-#     file_path = download_file(file)
-#     text = transcribe(file_path)
-#     resp = translate(text)
-    
-#     bot.send_message(text=resp, chat_id=userid, parse_mode="Markdown")
-
-
-# def handle_voice(update: Update, context):
-#     userid = update.message.from_user.id
-#     if str(userid) not in admins: 
-#         return
-
-#     file = bot.get_file(update.message.voice.file_id)
-    
-#     file_path = download_file(file)
-#     text = transcribe(file_path)
-#     resp = translate(text)
-
-#     bot.send_message(text=resp, chat_id=userid, parse_mode="Markdown")
 
 
 def main():
     # Create an instance of the Updater class using the bot token
     updater = Updater(token=bot_token, use_context=True)
     dp = updater.dispatcher
-
-    # # Create handler to capture audio messages
-    # dp.add_handler(MessageHandler(Filters.audio, handle_audio))
-
-    # # Create handler to capture audio messages
-    # dp.add_handler(MessageHandler(Filters.voice, handle_voice))
-
+    
     # Create handler for text messages
     dp.add_handler(MessageHandler(Filters.all, handle_all))
         
